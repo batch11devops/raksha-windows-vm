23530660b741aa0901c5f8fdfef64f5418132b02
batch11devops <rakshashetty722@gmail.com>
2025-08-17 22:36:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,6 +3,7 @@ resource "azurerm_resource_group" "rg" {
   location = "Japan East"
 }
 
+# Virtual Network
 resource "azurerm_virtual_network" "vnet" {
   name                = "win-vnet"
   address_space       = ["10.0.0.0/16"]
@@ -10,6 +11,7 @@ resource "azurerm_virtual_network" "vnet" {
   resource_group_name = azurerm_resource_group.rg.name
 }
 
+# Subnet
 resource "azurerm_subnet" "subnet" {
   name                 = "win-subnet"
   resource_group_name  = azurerm_resource_group.rg.name
@@ -17,15 +19,16 @@ resource "azurerm_subnet" "subnet" {
   address_prefixes     = ["10.0.1.0/24"]
 }
 
+# Public IP for VM
 resource "azurerm_public_ip" "publicip" {
-  name                = "myPublicIP"
+  name                = "win-public-ip"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Static"   
+  allocation_method   = "Static"
   sku                 = "Standard"
 }
 
-
+# NIC with Public IP
 resource "azurerm_network_interface" "nic" {
   name                = "win-nic"
   location            = azurerm_resource_group.rg.location
@@ -38,12 +41,15 @@ resource "azurerm_network_interface" "nic" {
     public_ip_address_id          = azurerm_public_ip.publicip.id
   }
 }
+
+# NSG
 resource "azurerm_network_security_group" "nsg" {
   name                = "win-nsg"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
 
+# Allow RDP
 resource "azurerm_network_security_rule" "rdp_allow" {
   name                        = "Allow-RDP"
   priority                    = 100
@@ -52,25 +58,26 @@ resource "azurerm_network_security_rule" "rdp_allow" {
   protocol                    = "Tcp"
   source_port_range           = "*"
   destination_port_range      = "3389"
-  source_address_prefixes     = ["48.210.81.176/32"]   # safer than Any
+  source_address_prefixes     = ["48.210.81.176/32"] # Restrict to your IP
   destination_address_prefix  = "*"
   resource_group_name         = azurerm_resource_group.rg.name
   network_security_group_name = azurerm_network_security_group.nsg.name
 }
 
-resource "azurerm_network_interface_security_group_association" "nic_nsg" {
-  network_interface_id      = azurerm_network_interface.nic.id
+# Associate NSG with Subnet (better than NIC level)
+resource "azurerm_subnet_network_security_group_association" "subnet_nsg" {
+  subnet_id                 = azurerm_subnet.subnet.id
   network_security_group_id = azurerm_network_security_group.nsg.id
 }
 
-
+# Windows VM
 resource "azurerm_windows_virtual_machine" "winvm" {
   name                  = "win-server"
   resource_group_name   = azurerm_resource_group.rg.name
   location              = azurerm_resource_group.rg.location
-  size                  = "Standard_D4s_v3" # 4c/16GB
+  size                  = "Standard_D4s_v3"
   admin_username        = "azureuser"
-  admin_password        = "Password@1234"
+  admin_password        = "Rks@3007"
   network_interface_ids = [azurerm_network_interface.nic.id]
 
   os_disk {
@@ -85,3 +92,4 @@ resource "azurerm_windows_virtual_machine" "winvm" {
     version   = "latest"
   }
 }
+
