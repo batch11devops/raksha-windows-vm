d0c3c755c81c906d3f01088f64e866b49a5141b6
batch11devops <rakshashetty722@gmail.com>
2025-08-17 17:45:30 +0530

    Create provider.tf
    adding provider.tf

---
+++ provider.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_version = ">= 1.6"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+  cloud {
+    organization = "raksha-terra-org"  
+    workspaces { name = "raksha-windows-vm" }     
+  }
+}
+
+provider "azurerm" {
+  features {}
+  
+}
