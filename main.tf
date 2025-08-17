61f5f457ce4c42f97195563ce697b173909ee37d
batch11devops <rakshashetty722@gmail.com>
2025-08-17 21:41:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -38,9 +38,34 @@ resource "azurerm_network_interface" "nic" {
     public_ip_address_id          = azurerm_public_ip.publicip.id
   }
 }
+resource "azurerm_network_security_group" "nsg" {
+  name                = "win-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_network_security_rule" "rdp_allow" {
+  name                        = "Allow-RDP"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "3389"
+  source_address_prefixes     = ["48.210.81.176/32"]   # safer than Any
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.rg.name
+  network_security_group_name = azurerm_network_security_group.nsg.name
+}
+
+resource "azurerm_network_interface_security_group_association" "nic_nsg" {
+  network_interface_id      = azurerm_network_interface.nic.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
 
 resource "azurerm_windows_virtual_machine" "winvm" {
-  name                  = "win-server"
+  name                  = "windows-raksha"
   resource_group_name   = azurerm_resource_group.rg.name
   location              = azurerm_resource_group.rg.location
   size                  = "Standard_D4s_v3" # 4c/16GB
