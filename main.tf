e97077e46df4b5aec2c7bba16823c0f8bc629130
batch11devops <rakshashetty722@gmail.com>
2025-08-17 18:45:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,34 +1,8 @@
-terraform {
-  required_version = ">= 1.6"
-
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 4.0"
-    }
-  }
-
-  cloud {
-    organization = "raksha-terra-org"
-    workspaces {
-      name = "raksha-windows-vm"
-    }
-  }
-}
-
-provider "azurerm" {
-  features {}
-  # No credentials here – they will come from environment variables:
-  # ARM_SUBSCRIPTION_ID, ARM_CLIENT_ID, ARM_CLIENT_SECRET, ARM_TENANT_ID
-}
-
-# Resource Group
 resource "azurerm_resource_group" "rg" {
   name     = "win-rg"
   location = "Japan East"
 }
 
-# Virtual Network
 resource "azurerm_virtual_network" "vnet" {
   name                = "win-vnet"
   address_space       = ["10.0.0.0/16"]
@@ -36,7 +10,6 @@ resource "azurerm_virtual_network" "vnet" {
   resource_group_name = azurerm_resource_group.rg.name
 }
 
-# Subnet
 resource "azurerm_subnet" "subnet" {
   name                 = "win-subnet"
   resource_group_name  = azurerm_resource_group.rg.name
@@ -44,9 +17,45 @@ resource "azurerm_subnet" "subnet" {
   address_prefixes     = ["10.0.1.0/24"]
 }
 
-# Public IP
 resource "azurerm_public_ip" "publicip" {
-  name                = "win-publicip"
+  name                = "win-public-ip"
   location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Dynamic"
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "win-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.publicip.id
+  }
+}
+
+resource "azurerm_windows_virtual_machine" "winvm" {
+  name                  = "win-server"
+  resource_group_name   = azurerm_resource_group.rg.name
+  location              = azurerm_resource_group.rg.location
+  size                  = "Standard_D4s_v3" # 4c/16GB
+  admin_username        = "azureuser"
+  admin_password        = "Password@1234"
+  network_interface_ids = [azurerm_network_interface.nic.id]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2019-Datacenter"
+    version   = "latest"
+  }
+}
 
